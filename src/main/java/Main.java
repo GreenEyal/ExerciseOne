8f9f208698476dc94bf802f6b507dbef71963938
Eyal <grineyal@gmail.com>
2020-04-06 11:20:53 +0300

    Initial Commit: Main flow & logic working. No seperation yet.

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import org.apache.commons.codec.binary.Base64InputStream;
+
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File(Main.class.getResource("file.txt.gz").getFile());
+        byte[] buffer = new byte[1024];
+        int signal;
+        String decodedString = "";
+        try {
+            InputStream fileInputStream = new FileInputStream(file);
+            GZIPInputStream gzipInputStream = new GZIPInputStream(fileInputStream);
+            Base64InputStream base64InputStream = new Base64InputStream(gzipInputStream);
+            signal = base64InputStream.read(buffer);
+            while (signal != -1) {
+                decodedString += new String(buffer);
+                signal = base64InputStream.read(buffer);
+            }
+            gzipInputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(decodedString);
+    }
+}
