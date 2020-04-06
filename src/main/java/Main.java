8c97a02ea99bd80027d14f0485d34581fa2f9a53
Eyal <grineyal@gmail.com>
2020-04-06 17:10:28 +0300

    Split up logic to methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,29 @@ import java.util.zip.GZIPInputStream;
 public class Main {
     public static void main(String[] args) {
         try {
-            PropertiesReader propertiesReader = new PropertiesReader();
-            Properties properties = propertiesReader.read("conf.properties");
-            File file = new File(Main.class.getResource(properties.getProperty("inputFile")).getFile());
-            InputStream fileInputStream = new FileInputStream(file);
-            GZIPInputStream gzipInputStream = new GZIPInputStream(fileInputStream);
-            Base64InputStream base64InputStream = new Base64InputStream(gzipInputStream);
-            InputStreamReader inputStreamReader = new InputStreamReader(base64InputStream);
-            FileOutputStream fileOutputStream = new FileOutputStream(properties.getProperty("outputFile"));
-            fileOutputStream.write(inputStreamReader.readStream());
+            writeOutput();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Properties readProperties() throws IOException {
+        PropertiesReader propertiesReader = new PropertiesReader();
+        return propertiesReader.read("conf.properties");
+    }
+
+    private static InputStreamReader createInputStreamPath(Properties properties) throws IOException {
+        File file = new File(Main.class.getResource(properties.getProperty("inputFile")).getFile());
+        FileInputStream fileInputStream = new FileInputStream(file);
+        GZIPInputStream gzipInputStream = new GZIPInputStream(fileInputStream);
+        Base64InputStream base64InputStream = new Base64InputStream(gzipInputStream);
+        return new InputStreamReader(base64InputStream);
+    }
+
+    private static void writeOutput() throws IOException {
+        Properties properties = readProperties();
+        InputStreamReader inputStreamReader = createInputStreamPath(properties);
+        FileOutputStream fileOutputStream = new FileOutputStream(properties.getProperty("outputFile"));
+        fileOutputStream.write(inputStreamReader.readStream());
     }
 }
