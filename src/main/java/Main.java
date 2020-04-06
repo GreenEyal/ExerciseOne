a5d013c05a6cb4400699393568b303009c4dfae9
Eyal <grineyal@gmail.com>
2020-04-06 11:31:21 +0300

    Added configuration file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import org.apache.commons.codec.binary.Base64InputStream;
 
 import java.io.*;
+import java.util.Properties;
 import java.util.zip.GZIPInputStream;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File(Main.class.getResource("file.txt.gz").getFile());
+        Properties properties = new Properties();
+        File file = new File(Main.class.getResource(properties.getProperty("file")).getFile());
         byte[] buffer = new byte[1024];
         int signal;
         String decodedString = "";
